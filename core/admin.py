1e6945417537ceedf78b3bccd238c416081b1edb
jordi <jmartinezmateos@iesesteveterradas.cat>
2019-02-18 17:57:47 +0100

    todos los ficheros

---
+++ core/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from core.models import *
+
+# Register your models here.
+
+class InlineSprint(admin.TabularInline):
+	model = Sprint
+	extra = 1
+
+class InlineSpec(admin.TabularInline):
+	model = Spec
+	extra = 1
+
+class ProjecteAdmin(admin.ModelAdmin):
+	inlines = [ InlineSprint, InlineSpec ]
+
+class SprintAdmin(admin.ModelAdmin):
+	list_display = ['projecte','data_inici','data_final','hores']
+
+class SpecAdmin(admin.ModelAdmin):
+	list_display = ['descripcio','dificultat','hores','projecte']
+
+admin.site.register(Projecte, ProjecteAdmin)
+admin.site.register(Spec,SpecAdmin)
+admin.site.register(Sprint,SprintAdmin)
